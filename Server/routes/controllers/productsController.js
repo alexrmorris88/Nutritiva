a7cdb839a87cd8b8315803e6ebb85fc52db094e1
Alex Morris <alexrmorris88@gmail.com>
2021-10-30 12:56:20 -0300

    added server product routes

--- Server/routes/controllers/productsController.js
+++ Server/routes/controllers/productsController.js
@@ -5,7 +5,7 @@ const Product = require("../../models/productSchema");
 
 // @route   POST /products/new
 // @desc    Create New Product
-// @access  Private
+// TODO: @access  Private 
 exports.newProduct = async (req, res, next) => {
   const product = await Product.create(req.body)
     .then((product) =>
@@ -15,4 +15,64 @@ exports.newProduct = async (req, res, next) => {
       })
     )
     .catch((err) => res.status(404).json(err.message));
+};
+
+// @route   GET /products/all
+// @desc    Display All Products
+// TODO: @access  Private
+exports.allProducts = async (req, res, next) => {
+  const product = await Product.find()
+    .then((product) =>
+      res.status(201).json({
+        success: true,
+        product,
+      })
+    )
+    .catch((err) => res.status(404).json(err.message));
+};
+
+// @route   GET /products/:id
+// @desc    Get Product by ID
+// TODO: @access  Private
+exports.getProductByID = async (req, res, next) => {
+
+  const product = await Product.findById(req.params.id)
+    .catch((err) => res.status(404).json(err.message));
+
+  if (!product) {
+    return next(res.status(404).json({
+      success: false,
+      error: "Product does not exist"
+    }))
+  };
+
+  res.status(200).json({
+    success: true,
+    product
+  })
+
+};
+
+// @route   DELETE /products/delete/:id
+// @desc    Display All Products
+// TODO: @access  Private
+exports.deleteProduct = async (req, res, next) => {
+
+  const product = await Product.findById(req.params.id)
+    .catch((err) => res.status(404).json(err.message));
+
+  if (!product) {
+    return next(res.status(404).json({
+      success: false,
+      error: "Product does not exist"
+    }))
+  };
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product successfully deleted"
+  })
+
 };
--- Server/routes/products.js
+++ Server/routes/products.js
@@ -2,12 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 // importing function from the controller
-const { newProduct } = require("./controllers/productsController");
+const {
+  newProduct,
+  allProducts,
+  deleteProduct,
+  getProductByID,
+} = require("./controllers/productsController");
 
 // New Product
-router
-  .route("/new")
-  .post(newProduct);
+router.route("/new").post(newProduct);
+
+// All Products
+router.route("/all").get(allProducts);
+
+// Get Product by ID
+router.route("/:id").get(getProductByID);
+
+// Delete Product
+router.route("/delete/:id").delete(deleteProduct);
 
 // Export Router
 module.exports = router;
