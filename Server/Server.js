43106448f7129aa9213a6a2921640abd86d62aff
Alex Morris <alexrmorris88@gmail.com>
2021-10-30 11:46:00 -0300

    Server is working

--- Server/Server.js
+++ Server/Server.js
@@ -1,9 +1,19 @@
+// Setup Express
 const express = require("express");
-
 const app = express();
+
+// Run backend server
+const dotenv = require("dotenv");
+
+// Setting up config file
+dotenv.config({ path: "Server/config/config.env" });
 
 app.get("/", (req, res) => res.send("hello"));
 
-const port = process.env.PORT;
+app.listen(process.env.PORT, () =>
+  console.log(
+    `The server is running on port ${process.env.PORT} in ${process.env.NODE_ENV}`
+  )
+);
 
-app.listen(port, () => console.log(`The server is running on port ${port}`));
+module.exports = app;
