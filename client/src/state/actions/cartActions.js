feba461b9c88f7907eaa92ca69554fd8aea88d60
alexrmorris88 <alexrmorris88@gmail.com>
2021-11-25 21:08:03 -0400

    added cart state

---
+++ client/src/state/actions/cartActions.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import {
+  ADD_TO_CART,
+  REMOVE_ITEM_FROM_CART,
+  SAVE_SHIPPING_INFO,
+} from "../constants/cartConstants";
+
+// Add items to cart
+export const addItemToCart = (id, quantity) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/product/prod/${id}`);
+
+  dispatch({
+    type: ADD_TO_CART,
+    payload: {
+      product: data.product._id,
+      name: data.product.name,
+      price: data.product.price,
+      image: data.product.image,
+      stock: data.product.stock,
+      quantity,
+    },
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+// Remove items from cart
+// This is not an API Call, we are only deleting the item from local sotrage
+export const removeItemFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: REMOVE_ITEM_FROM_CART,
+    payload: id,
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+// Save Shipping Info
+export const saveShippingInfo = (data) => async (dispatch) => {
+  dispatch({
+    type: SAVE_SHIPPING_INFO,
+    payload: data,
+  });
+
+  localStorage.setItem("shippingInfo", JSON.stringify(data));
+};
